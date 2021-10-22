6d61f28e63badf7fb9b4901267acb6d48621e195
Kris <mrocyka.chris"gmail.com>
2021-10-22 10:47:28 +0200

    ViewResolver Added

--- src/main/java/academy/learnprogramming/controller/DemoController.java
+++ src/main/java/academy/learnprogramming/controller/DemoController.java
@@ -13,4 +13,10 @@ public class DemoController {
     public String hello(){
         return "hello";
     }
+
+    // http://localhost:8080/todo-list/welcome
+    @GetMapping("welcome")
+    public String welcome () {
+        return "welcome";
+    }
 }
